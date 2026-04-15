73a7df615a34842bf86cdd6a0e3cd9d734030b5d
EeveeningRain <evelynwillow.DEV@gmail.com>
2026-04-15 19:04:49 +1000

    added basic header and main funcs to be filled out

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include "main.h"
 
 int main(void){
+    /*get in some kind of file?*/
+    /*
+     * encrypt,
+     * compress,
+     *
+     * ?
+     *
+     * decompress,
+     * de-encrypt */
+    return 0;
+}
+
+int encrypt_file(FILE* file){
+    return 0;
+}
+
+int deencrypt_file(FILE* file){
+    return 0;
+}
+
+int compress_file(FILE* file){
+    return 0;
+}
+
+int decompress_file(FILE* file){
     return 0;
 }
--- main.h
+++ main.h
@@ -3,3 +3,11 @@
 #include <string.h>
 #include <math.h>
 
+int encrypt_file(FILE* file); /*returns 0 or -1 for success of encryption, store somewhere? */
+
+int deencrypt_file(FILE* file); /* returns 0 or -1 for success, store somewhere? */
+
+int compress_file(FILE* file); /* returns 0 or -1 for success, store somewhere? */
+
+int decompress_file(FILE* file); /* returns 0 or -1 for success, store somewhere? */
+
